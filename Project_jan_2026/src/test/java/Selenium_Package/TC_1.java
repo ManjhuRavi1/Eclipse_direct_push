4921a6819f1540594f1b587f7c2386ddd825e4d1
Ravindra <ravimanjhu1123@gmail.com>
2026-01-20 16:31:32 +0530

    20th Jan 2026

---
+++ Project_jan_2026/src/test/java/Selenium_Package/TC_1.java
@@ -0,0 +1,33 @@
+package Selenium_Package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com/");
+		
+		WebElement search = driver.findElement(By.id("APjFqb"));
+		search.sendKeys("Test Methods");
+		
+		//WebElement searchbutton = driver.findElement(By.name("btnk");
+		//searchbutton.click();
+		search.submit();	
+		
+		System.out.println("Title is : " + driver.getTitle());
+		driver.close();
+		
+		
+	}
+
+}
